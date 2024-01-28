052c024035a5abeccee08aedc25c67d011679b85
Sahad Mp <muhdsahad4916@gmail.com>
2024-01-28 17:23:22 +0530

    done addmin side complait ui

---
+++ front_end/path_way_flu/lib/features/admin/presentation/pages/complant_list.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:path_way_flu/core/constants/constants.dart';
+
+class ComplantList extends StatelessWidget {
+  const ComplantList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Complaint',
+            style: kHeadingextStyle,
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
+            bottom: 20,
+            top: 30,
+          ),
+          child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    height: 150,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 235, 235, 235),
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.grey,
+                          size: 20,
+                        ),
+                        const SizedBox(width: 15),
+                        ConstrainedBox(
+                          constraints: const BoxConstraints(
+                            maxHeight: 200,
+                            maxWidth: 250,
+                          ),
+                          child: const SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Title of the complaint show here",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16,
+                                    color: Color.fromARGB(255, 62, 62, 62),
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                    color: Colors.blueGrey,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }),
+        ));
+  }
+}
--- front_end/path_way_flu/lib/main.dart
+++ front_end/path_way_flu/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:path_way_flu/features/admin/presentation/pages/list_of_tutorial.dart';
+import 'package:path_way_flu/features/admin/presentation/pages/complant_list.dart';
 import 'package:path_way_flu/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(),
-        home: const ListOfTutorial(),
+        home: const ComplantList(),
       ),
     );
   }
